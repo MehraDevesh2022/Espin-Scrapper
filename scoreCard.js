558fb06f6b73efcb695700646146ed41ff7dc0ea
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-17 13:40:34 +0530

    teamName and opponentName added

--- scoreCard.js
+++ scoreCard.js
@@ -1,50 +1,52 @@
 const url =
   "https://www.espncricinfo.com/series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
 
-  const request = require('request');
-  const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 
+request(url, cb);
 
-  request(url , cb);
-  
-  function cb(error , response , html){
-   if(error){
+function cb(error, response, html) {
+  if (error) {
     console.log(error);
-   }else{
+  } else {
     getScore(html);
-
-   }
+  }
 }
 
-function getScore(html){
-let $ =  cheerio.load(html);
-// get venu and date of match :
-let data = $(".ds-text-tight-m.ds-font-regular.ds-text-ui-typo-mid");
-let getData = $(data).text();
+function getScore(html) {
+  let $ = cheerio.load(html);
+  // get venue and date of match :
+  let data = $(".ds-text-tight-m.ds-font-regular.ds-text-ui-typo-mid");
+  let getData = $(data).text();
 
-// convert str to array  geting venu and date sapreatly
+  // convert str to array  geting venue and date sapreatly
 
-getData = getData.split(',');
-let venu = getData[1].trim();
-let date = getData[2].trim();
+  getData = getData.split(",");
+  let venue = getData[1].trim();
+  let date = getData[2].trim();
 
-// now get result of the match : =>
-let result = $(
-  ".ds-text-tight-m.ds-font-regular.ds-truncate.ds-text-typo-title"
-).text();
+  // now get result of the match : =>
+  let result = $(
+    ".ds-text-tight-m.ds-font-regular.ds-truncate.ds-text-typo-title"
+  ).text();
 
+  console.log(venue);
+  console.log(date);
+  console.log(result);
+  console.log("-------------------------------------------------");
+  let innings = $(".ds-rounded-lg.ds-mt-2");
+  console.log(innings.length);
+  let htmlStr = "";
+  for (let i = 0; i < innings.length; i++) {
+    htmlStr += $(innings[i]).html();
+    let teamName = $(innings[i]).find(".ds-text-title-xs.ds-font-bold.ds-capitalize").text();
 
-console.log(venu);
-console.log(date);
-console.log(result);
+    let apponentName = i == 0 ? 1 : 0;
+    apponentName = $(innings[apponentName]).find(".ds-text-title-xs.ds-font-bold.ds-capitalize").text();
 
-let innings = $(".ds-rounded-lg.ds-mt-2");
-console.log(innings.length);
-let htmlStr =''
-for(let i=0; i<innings.length; i++){
- htmlStr += $(innings[i]).html();
+    console.log(`venue : ${venue}  date : ${date} teams : ${teamName} vs ${apponentName}`);
+
+  }
+  // console.log(htmlStr);
 }
-console.log(htmlStr);
-}
-
-
